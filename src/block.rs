fc696dfab79d3ec9ed45c1bd2a161ace32de3462
RahulVivekNair <68507071+RahulVivekNair@users.noreply.github.com>
2022-12-26 23:02:01 +0530

    init block structure

--- src/block.rs
+++ src/block.rs
@@ -1,6 +1,12 @@
 use std::fmt::{ self, Debug, Formatter };
-
+use super::*;
 pub struct Block {
+    pub index: u32,
+    pub timestamp: u128,
+    pub hash: BlockHash,
+    pub previous_hash: BlockHash,
+    pub nonce: u64,
+    pub payload: String,
 }
 
 impl Debug for Block {
@@ -10,4 +16,14 @@ impl Debug for Block {
 }
 
 impl Block {
+    pub fn new() -> Self {
+        Self {
+            index: 0,
+            timestamp: now(),
+            hash: vec![],
+            previous_hash: vec![],
+            nonce: 0,
+            payload: String::new(),
+        }
+    }
 }
